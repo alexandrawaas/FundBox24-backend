5d2ce6e3ac58b5fb83226985c5c9483838d0a597
Tim <timwagner997@gmail.com>
2024-05-24 13:00:01 +0200

    added a user details service

--- src/main/java/com/example/fundbox24backend/config/BasicAuthConfig.java
+++ src/main/java/com/example/fundbox24backend/config/BasicAuthConfig.java
@@ -3,6 +3,10 @@ package com.example.fundbox24backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -11,12 +15,23 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class BasicAuthConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                (authz) -> authz
+        http
+                .authorizeHttpRequests((authorize) -> authorize
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
