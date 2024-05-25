fce8fd17f870e10bc6ffa37eb9c156186e288626
Tim <timwagner997@gmail.com>
2024-05-26 00:27:26 +0200

    disabled CSRF

--- src/main/java/com/example/fundbox24backend/config/AuthConfig.java
+++ src/main/java/com/example/fundbox24backend/config/AuthConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,8 +22,10 @@ public class AuthConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                authorize -> authorize
+        http
+                // WARNING: Disabling CSRF is not good, but I disabled it here for simplicity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults())
