08c51356c421869dbeb7634b95a2a6717c3fe400
Tim <timwagner997@gmail.com>
2024-05-25 21:42:31 +0200

    added a new AuthService

---
+++ src/main/java/com/example/fundbox24backend/api/service/AuthService.java
@@ -0,0 +1,38 @@
+package com.example.fundbox24backend.api.service;
+
+import com.example.fundbox24backend.api.model.User;
+import com.example.fundbox24backend.api.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class AuthService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    /*
+    INFO: username is email
+     */
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
