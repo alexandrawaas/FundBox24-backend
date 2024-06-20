80ddb68f5c0bdeb6fee62dfad3dda8fd457b0c67
Tim <timwagner997@gmail.com>
2024-06-20 12:28:50 +0200

    assert that userController isn't null

---
+++ src/test/java/com/example/fundbox24backend/LoginTest.java
@@ -0,0 +1,20 @@
+package com.example.fundbox24backend;
+
+import com.example.fundbox24backend.api.controller.UserController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class LoginTest {
+
+    @Autowired
+    private UserController userController;
+
+    @Test
+    void contextLoads() {
+        assertThat(userController).isNotNull();
+    }
+}
