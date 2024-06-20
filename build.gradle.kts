2de3002a5692448e0e0573caab752a762ddf2c1c
Tim <timwagner997@gmail.com>
2024-06-20 12:28:30 +0200

    added AssertJ dependency for extra assertions

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,9 @@ dependencies {
     // developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.assertj:assertj-core:3.11.1")
 }
 
 tasks.withType<Test> {
